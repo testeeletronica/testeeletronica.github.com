c884e9bc9683aee51b33cccaad62f49f71c880f3
Pedro <pedro_psantana@hotmail.com>
2022-09-05 21:45:29 -0300

    teste5

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var appForm = document.getElementById('app-form');
+
 var listaPessoas = document.getElementById('listaPessoas');
+
 var btnOrdenar = document.getElementById('btnOrdenar');
 
 var pessoas = [];
@@ -13,7 +15,9 @@ function addPessoa(e){
 	console.log(e);
 
 	var nome = e.target.pessoaNome.value;
+
 	var sobrenome = e.target.pessoaSobrenome.value;
+
 	var telefone = e.target.pessoaTelefone.value
 
 	var pessoa = { nome, sobrenome, telefone };
@@ -37,14 +41,17 @@ function validarCampos(pessoa){
 		validation.status = false;
 		validation.error = 'Preencha o campo Nome';
 	}
+
 	else if(pessoa.sobrenome.length === 0){
 		validation.status = false;
 		validation.error = 'Preencha o campo Sobrenome';
 	}
+
 	else if(pessoa.telefone.length < 10){
 		validation.status = false;
 		validation.error = 'Preencha o campo Telefone corretamente';
 	}
+
 	return validation;
 }
 
@@ -72,9 +79,13 @@ function mostrarLista(){
 		alterarEl.setAttribute('onclick', 'alterarPessoa(' + indice + ')');
 
 		var itemEl = document.createElement('li');
+
 		itemEl.appendChild(nomeEl);
+
 		itemEl.appendChild(telefoneEl);
+
 		itemEl.appendChild(alterarEl);
+
 		itemEl.appendChild(removerEl);
 
 		listaPessoas.appendChild(itemEl);
@@ -88,16 +99,23 @@ function removerPessoa(indice){
 
 function alterarPessoa(indice){
 	var btnCadastrar = document.getElementById('btnCadastrar');
+
 	var btnEditar = document.getElementById('btnEditar');
+
 	var input_nome = document.getElementById('pessoaNome');
+
 	var input_sobrenome = document.getElementById('pessoaSobrenome');
+
 	var input_telefone = document.getElementById('pessoaTelefone');
 
 	btnCadastrar.setAttribute('style', 'display:none');
+
 	btnEditar.setAttribute('style', 'display:');
 
 	input_nome.value = pessoas[indice].nome;
+
 	input_sobrenome.value = pessoas[indice].sobrenome;
+
 	input_telefone.value = pessoas[indice].telefone;
 
 	btnEditar.onclick = function(){
@@ -114,10 +132,13 @@ function alterarPessoa(indice){
 		}
 
 		input_nome.value = '';
+
 		input_sobrenome.value = '';
+
 		input_telefone.value = '';
 
 		btnCadastrar.setAttribute('style', 'display:');
+
 		btnEditar.setAttribute('style', 'display:none');
 
 		pessoas[indice] = pessoaAlterada;
@@ -128,10 +149,15 @@ function alterarPessoa(indice){
 function ordenarLista(){
 	pessoas.sort(function(a, b){
 		var x = a.nome.toLowerCase() + a.sobrenome.toLowerCase();
+
 		var y = b.nome.toLowerCase() + b.sobrenome.toLowerCase();
+
 		if(x < y) return -1;
+
 		if(x > y) return 1;
+
 		return 0;
 	});
+	
 	mostrarLista();
 }
